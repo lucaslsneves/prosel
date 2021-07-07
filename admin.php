5622836c3d45887a02d0d4e46a003899f351cee2
Lucas Luz de Sena Neves <lucas.neves@ints.local>
2021-07-07 13:31:10 -0300

    add favicon do admin.php page

--- admin.php
+++ admin.php
@@ -6,7 +6,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Control-panel Login</title>
+    <title>Login</title>
+    <link rel="icon" href="assets/favicon.png" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.10/jquery.mask.js"></script>
 </head>
