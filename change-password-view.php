54bc337ced460d421ed8060dccd6448bc0e035df
Lucas Luz <lucasluzthatsme@gmail.com>
2021-09-23 13:51:55 -0300

    change password view css

--- change-password-view.php
+++ change-password-view.php
@@ -4,19 +4,19 @@ require "verifica.php";
 
 
 <form style="display:flex; flex-direction:column; margin-top:16px;" id="form-password">
-    <div class="own-form-field">
-        <label for="current-password">Senha Atual</label>
-        <input id="current-password" type="password">
+    <div  style="margin-bottom:16px;" class="own-form-field">
+        <label style="margin-bottom:8px;" for="current-password">Senha Atual</label>
+        <input style="padding:8px;" id="current-password" type="password">
     </div>
 
-    <div class="own-form-field">
-        <label for="new-password">Nova Senha</label>
-        <input id="new-password" type="password">
+    <div  style="margin-bottom:16px;" class="own-form-field">
+        <label  style="margin-bottom:8px;" for="new-password">Nova Senha</label>
+        <input style="padding:8px;"id="new-password" type="password" placeholder="No mínimo 8 caracteres">
     </div>
 
-    <div class="own-form-field">
-        <label for="new-password-confirmation">Confirmar Nova Senha</label>
-        <input id="new-password-confirmation" type="password">
+    <div  style="margin-bottom:0;" class="own-form-field">
+        <label  style="margin-bottom:8px;" for="new-password-confirmation">Confirmar Nova Senha</label>
+        <input style="padding:8px;"id="new-password-confirmation" type="password" placeholder="No mínimo 8 caracteres">
     </div>
     <button class="submit-button">Alterar</button>
 </form>
